795405f24d2dc9f0c3b0caef15609a845410ae32
Daryl <dyk0203@naver.com>
2022-11-01 01:47:28 +0900

    axios request params config

--- src/data/api.js
+++ src/data/api.js
@@ -1,3 +1,27 @@
+import axios from "axios";
+
 export const getExpenditureList = async () => {
-  return await (await fetch("/list")).json();
+  const config = {
+    method: "post",
+    url: `/list`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    params: {
+      data: JSON.stringify({
+        data: {
+          a: "1",
+          b: "2",
+        },
+      }),
+    },
+    // data: { data: 1 },
+  };
+  const { data } = await axios(config);
+
+  console.log(data);
+  return data;
+  // return await (
+  //   await fetch("/list", { body: JSON.stringify({ data: 1 }) })
+  // ).json();
 };
