5c33aafadc8c89f324d1dfe02b519f4e1dde8205
Daryl <dyk0203@naver.com>
2022-11-24 01:54:05 +0900

    최근 3개월간 거래내역 리스트

---
+++ src/components/accountBook/RecentSpendHistory.js
@@ -0,0 +1,115 @@
+import { getSpendList3MonthById } from "data/api";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import styled from "styled-components";
+import { settingProperties } from "utils/property";
+
+const Wrapper = styled.div`
+  background-color: white;
+`;
+const OverviewInfo = styled.div`
+  margin: 10px;
+  padding: 20px;
+`;
+const OverviewTitle = styled.span`
+  font-size: 20px;
+`;
+const OverviewGroup = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+`;
+const Overview = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+const OverviewTr = styled.span``;
+const OverviewTd = styled.span`
+  font-weight: 700;
+`;
+
+const List = styled.ul``;
+const SpendCard = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 10px;
+  padding: 10px;
+  gap: 10px;
+`;
+const CardMark = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  background-color: #3498db;
+`;
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+const Price = styled.span`
+  font-size: 18px;
+`;
+const Store = styled.span`
+  font-size: 14px;
+`;
+
+const RecentSpendHistory = ({ mainCategory, subCategory, store }) => {
+  const [recentHistory, setRecentHistory] = useState([]);
+  const { isLoading, data } = useQuery("getSpendList3MonthById", () =>
+    getSpendList3MonthById({ mainCategory, subCategory })
+  );
+
+  useEffect(() => {
+    setRecentHistory(
+      data?.results
+        .map((item) => {
+          return {
+            id: item.id,
+            ...settingProperties(item.properties),
+          };
+        })
+        .filter((item) => item.store === store)
+    );
+  }, [data]);
+
+  return (
+    <Wrapper>
+      <OverviewInfo>
+        <OverviewTitle>최근 3개월간 거래 내역</OverviewTitle>
+        <OverviewGroup>
+          <Overview>
+            <OverviewTr>거래 횟수</OverviewTr>
+            <OverviewTd>{recentHistory.length} 회</OverviewTd>
+          </Overview>
+          <Overview>
+            <OverviewTr>총 금액</OverviewTr>
+            <OverviewTd>
+              {`- ${recentHistory
+                .map((item) => item.withdraw)
+                .reduce((a, b) => Number(a) + Number(b), [])} 원`}
+            </OverviewTd>
+          </Overview>
+        </OverviewGroup>
+      </OverviewInfo>
+      <List>
+        {recentHistory.map((spend) => {
+          return (
+            <SpendCard key={spend.id}>
+              <CardMark></CardMark>
+              <CardContent>
+                <Price>{`- ${spend.withdraw} 원`}</Price>
+                <Store>{spend.store}</Store>
+              </CardContent>
+            </SpendCard>
+          );
+        })}
+      </List>
+    </Wrapper>
+  );
+};
+
+export default RecentSpendHistory;
--- src/pages/AccountBook/spend/Detail.js
+++ src/pages/AccountBook/spend/Detail.js
@@ -1,13 +1,10 @@
-import { useEffect, useState } from "react";
-import { useQuery } from "react-query";
 import { useNavigate, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 
 import { spendDetailSelector } from "data/atoms/spend";
-import { getSpendList3MonthById } from "data/api";
-import { settingProperties } from "utils/property";
 import DetailInfo from "components/accountBook/DetailInfo";
+import RecentSpendHistory from "components/accountBook/RecentSpendHistory";
 
 const Wrapper = styled.div``;
 const Header = styled.div`
@@ -16,6 +13,7 @@ const Header = styled.div`
   align-items: center;
   padding: 10px;
   margin-top: 5px;
+  background-color: white;
 `;
 const PreviousButton = styled.span`
   display: flex;
@@ -24,32 +22,11 @@ const PreviousButton = styled.span`
 `;
 const Title = styled.span``;
 const Content = styled.div``;
-// TODO
-const RecentHistory = styled.div``;
 
 const Detail = () => {
   const { type, id } = useParams();
   const navigate = useNavigate();
   const spendInfo = useRecoilValue(spendDetailSelector({ type, id }));
-  const [recentHistory, setRecentHistory] = useState([]);
-
-  const { isLoading, data } = useQuery("getSpendList3MonthById", () =>
-    getSpendList3MonthById({
-      mainCategory: spendInfo.mainCategory,
-      subCategory: spendInfo.subCategory,
-    })
-  );
-
-  useEffect(() => {
-    setRecentHistory(
-      data?.results.map((item) => {
-        return {
-          id: item.id,
-          ...settingProperties(item.properties),
-        };
-      })
-    );
-  }, [data]);
 
   return (
     <Wrapper>
@@ -60,8 +37,11 @@ const Detail = () => {
       </Header>
       <Content>
         <DetailInfo {...spendInfo} />
-        {/* TODO */}
-        <RecentHistory></RecentHistory>
+        <RecentSpendHistory
+          mainCategory={spendInfo.mainCategory}
+          subCategory={spendInfo.subCategory}
+          store={spendInfo.store}
+        />
       </Content>
     </Wrapper>
   );
